c09c3b0e40a4a18dc7eccd4c2e83872075be3950
juswink <justin.wong@puppet.com>
2021-09-01 18:18:41 +0800

    Windows base config

--- site-modules/profile/manifests/platform/windows.pp
+++ site-modules/profile/manifests/platform/windows.pp
@@ -2,8 +2,14 @@ class profile::platform::windows::base {
 
   user { 'Art Vandelay':
     ensure => 'present',
-    name => 'ArtVandelay',
+    name   => 'ArtVandelay',
     groups => 'Administrators',
   }
 
+ file { 'C:/adminTools':
+    ensure => 'file',
+    mode   => '0650',
+    owner  => 'ArtVandelay',
+    group  => 'Administrators',
+  }
 }
