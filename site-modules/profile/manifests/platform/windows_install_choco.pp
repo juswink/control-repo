ef0cd4f97fa8922c60ac712f4f8db22259d888e5
juswink <justin.wong@puppet.com>
2021-09-02 16:17:22 +0800

    Windows base config

--- site-modules/profile/manifests/platform/windows_install_choco.pp
+++ site-modules/profile/manifests/platform/windows_install_choco.pp
@@ -3,4 +3,8 @@ class profile::platform::windows::base::install::choco {
   use_7zip => false,
 
   include chocolatey
+
+  exec { '7-zip':
+    command => 'C:\ProgramData\chocolatey\choco.exe install 7zip',
+  }
 }
