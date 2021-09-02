f164d5547b3aaaeb702935f61a344a954c2a6d9e
juswink <justin.wong@puppet.com>
2021-09-02 13:57:45 +0800

    Windows base config

--- site-modules/profile/manifests/platform/windows.pp
+++ site-modules/profile/manifests/platform/windows.pp
@@ -56,6 +56,7 @@ class profile::platform::windows::base {
 
   archive { 'MobaXterm':
     ensure => present,
+    path => 'C:/Temp/MobaXterm_Installer_v21.3.zip',
     extract => true,
     extract_path => 'C:/Temp',
     source => 'https://download.mobatek.net/2132021082033134/MobaXterm_Installer_v21.3.zip',
