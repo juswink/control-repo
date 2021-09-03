322908792f8abbed36bbd0da40252bbcfbddab39
juswink <justin.wong@puppet.com>
2021-09-03 14:54:14 +0800

    Windows base config

--- site-modules/profile/manifests/application/webserver/iis.pp
+++ site-modules/profile/manifests/application/webserver/iis.pp
@@ -1,5 +1,10 @@
 class profile::application::webserver::iis (
   Boolean $default_website = true,
+  String $doc_root           = 'C:\inetpub\wwwroot\sample_website',
+  Integer $webserver_port    = 80,   # change this default value in Hiera common.yaml
+  String $apppool            = 'sample_website',
+  String $website_source_dir = 'puppet:///modules/profile/app/sample_website',
+
 ){
 
   if $::kernel != 'windows' {
@@ -23,6 +28,31 @@ class profile::application::webserver::iis (
       ensure => 'absent',
     }
   }
+
+  iis_site { 'sample_website':
+    ensure          => 'started',
+    physicalpath    => $doc_root,
+    applicationpool => $apppool,
+    bindings        => [
+      {
+         'bindinginformation'   => "*:$webserver_port:",
+         'protocol'             => 'http',
+      },
+    ],
+  }
+
+  file { $website_source_dir:
+    ensure  => directory,
+    path    => $doc_root,
+    #source  => $website_source_dir,
+    recurse => true,
+  }
+
+  file { "${doc_root}/index.html":
+    ensure  => file,
+    #content => epp('profile/app/sample_website.html.epp'),
+  }
+
 
 }
 
